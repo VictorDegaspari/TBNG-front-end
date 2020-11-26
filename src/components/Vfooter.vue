1455eb700bed689f8f9924611627b646ea12a0e6
VictorDegaspari <vdegaspari.vm@gmail.com>
2020-11-26 20:26:53 -0300

    footer

--- src/components/Vfooter.vue
+++ src/components/Vfooter.vue
@@ -1,11 +1,28 @@
 <template>
-  <footer><img src="../assets/rainbow-6-siege-1.svg" alt="" /></footer>
+  <footer>
+    <!-- <img src="../assets/rainbow-6-siege-1.svg" alt="" /><img
+      src="../assets/PlayStation_logo.svg"
+      alt=""
+    /> -->
+    <p>TBNG © todos os direitos reservados</p>
+  </footer>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
 footer {
+  background: linear-gradient(
+    to right,
+    rgba(0, 26, 110, 0.397),
+    rgba(35, 105, 255, 0.493)
+  );
   display: flex;
-  height: 50px;
-  z-index: 10;
+  align-items: center;
+  justify-content: center;
+  min-height: 60px;
   bottom: 50px;
+  width: 100%;
+  padding: 5px 10px 5px 10px;
+  p {
+    color: white;
+  }
 }
 </style>
