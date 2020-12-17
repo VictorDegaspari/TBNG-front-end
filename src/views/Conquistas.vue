241f5740c472a127eaff0fe8a95e4b0e9f77eda8
VictorDegaspari <vdegaspari.vm@gmail.com>
2020-12-17 15:24:13 -0300

    fix: change icons

--- src/views/Conquistas.vue
+++ src/views/Conquistas.vue
@@ -3,32 +3,48 @@
     <Titles msg="Maiores elos Rainbow Six"
       ><img id="icon-member" src="../assets/trofeu.svg" alt="" />
       <Container>
-        <Members texto="Victor Degaspari - season 6">
-          <img src="../assets/plat.svg" class="card-img-top" alt="..."
-        /></Members>
-        <Members texto="Renato Rodrigues - season 6"
-          ><img src="../assets/plat2.svg" class="card-img-top" alt="..."
-        /></Members>
-        <Members texto="Raphael Gomes - season 6"
-          ><img src="../assets/gold1.svg" class="card-img-top" alt="..."
-        /></Members>
-        <Members texto="Caio Domarco - season 6"
-          ><img src="../assets/gold1.svg" class="card-img-top" alt="..."
-        /></Members>
+        <Members texto="Victor Degaspari - season Shifting Tides">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
+        <Members texto="Renato Rodrigues - season 6">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
+        <Members texto="Raphael Gomes - season 6">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat2" />
+          </Titles>
+        </Members>
+        <Members texto="Caio Domarco - season 7">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
       </Container>
       <Container>
-        <Members texto="Leo Moretti - season 6"
-          ><img src="../assets/gold1.svg" class="card-img-top" alt="..."
-        /></Members>
-        <Members texto="Caio Di Bene - season 6"
-          ><img src="../assets/gold1.svg" class="card-img-top" alt="..."
-        /></Members>
-        <Members texto="Felipe Trevisan - season 6"
-          ><img src="../assets/gold1.svg" class="card-img-top" alt="..."
-        /></Members>
-        <Members texto="Matheus Cazelato - season 6"
-          ><img src="../assets/gold1.svg" class="card-img-top" alt="..."
-        /></Members>
+        <Members texto="Leo Moretti - season 6">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
+        <Members texto="Caio Di Bene - season 6">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
+        <Members texto="Felipe Trevisan - season 6">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
+        <Members texto="Matheus Cazelato - season 6">
+          <Titles msg="Top MMR 3330" :style="'background: #2c3e50'">
+            <img :src="plat3" />
+          </Titles>
+        </Members>
       </Container>
     </Titles>
   </div>
@@ -39,12 +55,22 @@
 import Members from "@/components/Vmembers.vue";
 import Container from "@/components/Vcontainers.vue";
 import Titles from "@/components/Vtitles.vue";
+import plat from "@/assets/plat.svg";
+import plat2 from "@/assets/platinum-2.svg";
+import plat3 from "@/assets/platinum-3.svg";
 
 export default {
   components: {
     Members,
     Container,
     Titles
+  },
+  data() {
+    return {
+      plat: plat,
+      plat2: plat2,
+      plat3: plat3
+    };
   }
 };
 </script>
@@ -59,5 +85,9 @@ export default {
   max-width: 120.7px;
   max-height: 120.7px;
   margin: 10px;
+}
+img {
+  width: 100%;
+  height: 250px;
 }
 </style>
