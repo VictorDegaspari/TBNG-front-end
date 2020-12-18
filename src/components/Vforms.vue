c47b16285595f078bbdde4f13f11b73998dae091
VictorDegaspari <vdegaspari.vm@gmail.com>
2020-12-18 10:36:12 -0300

    add require on inputs

--- src/components/Vforms.vue
+++ src/components/Vforms.vue
@@ -8,6 +8,7 @@
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
+          required
         />
         <small id="emailHelp" class="form-text text-muted"
           >We'll never share your email with anyone else.</small
@@ -19,6 +20,7 @@
           type="password"
           class="form-control"
           id="exampleInputPassword1"
+          required
         />
       </div>
       <div class="form-group form-check">
